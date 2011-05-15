9fb0ef239ffe0b6b8ca6421db246d76f79723d0e
Ben Ridder <brridder@gmail.com>
2011-05-15 15:19:15 -0400

    Ben - fixed timer, started on p1 documentation

--- timer0/timer.c
+++ timer0/timer.c
@@ -102,7 +102,7 @@ int main (void) {
         rtx_dbug_outs((char*) "loop\r\n");
 #endif
         if (last_counter != counter) {
-            rtx_dbug_outs((char*) "        \r");
+            rtx_dbug_outs((char*) "        \r"); // :S
             // Split up our counter into something more human readable.
             hours = (counter/3600) - (counter % 3600)/3600;
             seconds = counter - hours*3600;
