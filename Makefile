7156d2a8ee258373422c2c507a96c9ba21de4e4f
Ben Ridder <ben@xtremelabs.com>
2011-06-23 18:32:52 -0400

    * - trying to get the make files to work again

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 # Makefile
 include Makefile.inc
 
-DIRS=loader
+DIRS=loader tests
 ASM=start.s
 LDFLAGS = -Trtx.ld -Wl,-Map=main.map
 DEPS=dbug.h kernel.h soft_interrupts.h rtx.h system_processes.h init.h string.h process.h loader/rtx_test.h
 OBJS=dbug.o kernel.o soft_interrupts.o rtx.o system_processes.o init.o string.o main.o 
-TESTS=rtx_test_dummy.s19
+TESTS=rtx_test_dummy.s19 mem_tests.s19 priority_tests.s19
 
 all: tests 
 
@@ -19,8 +19,11 @@ rtx.s19: $(OBJS)
 rtx_loader.s19:
 	cd loader; $(MAKE) rtx_loader.s19;
 
-rtx_test_dummy.s19:
-	cd tests; $(MAKE) rtx_test_dummy.s19;
+#rtx_test_dummy.s19:
+#	cd tests; $(MAKE) rtx_test_dummy.s19;
+
+priority_tests.s19:
+	cd tests: $(MAKE) priority_tests.s19;
 
 main.s19: rtx.s19 rtx_loader.s19
 	$(ECHO) Merging rtx with loader...
@@ -38,13 +41,17 @@ main.s19: rtx.s19 rtx_loader.s19
 	  sed -e 's/^ *//' -e 's/$$/:/' >> $*.d
 	@rm -f $*.d.tmp
 
-.PHONY: clean tests
+.PHONY: clean tests build_tests
 clean:
 	rm -f *.s19 *.o *.bin *.map *.lst *.d
 	@for d in $(DIRS); do (cd $$d; $(MAKE) clean); done
 
-tests: main.s19 $(TESTS)
+build_tests: 
+	cd tests; $(MAKE);	
+
+tests: main.s19 build_tests
 	$(ECHO) Making tests
-	@for test in $(TESTS); do ( $(MERGE) full_$$test tests/$$test main.s19; \
+	@for test in $(TESTS); do ( $(ECHO) Making full_$$test; \
+	$(MERGE) full_$$test tests/$$test main.s19; \
 	chmod u+x full_$$test); done
 	
---
+++ tests/priority_tests.c
@@ -0,0 +1,116 @@
+/*--------------------------------------------------------------------------
+ *                      RTX Test Suite 
+ *--------------------------------------------------------------------------
+ */
+/**
+ * @file:   rtx_test_dummy.c   
+ * @author: Thomas Reidemeister
+ * @author: Irene Huang
+ * @date:   2010.02.11
+ * @brief:  rtx test suite 
+ */
+
+#include "rtx_test.h"
+#include "../dbug.h"
+#include "../string.h"
+#include "../rtx.h"
+/* third party dummy test process 1 */ 
+void test1() {
+    int i;
+    i = 0;
+    rtx_dbug_outs((CHAR *)"rtx_test: test1\r\n");
+
+    set_process_priority(1, 0);
+    set_process_priority(2, 0);
+    while (1) {
+        i++;
+        if (i >= 5) {
+            set_process_priority(1,4);
+            set_process_priority(1,4);
+            printf_0("Process starvation complete");
+        }
+        g_test_fixture.release_processor();
+    }
+}
+
+/* third party dummy test process 2 */ 
+void test2()
+{
+    rtx_dbug_outs((CHAR *)"rtx_test: test2\r\n");
+    while (1) 
+    {
+        /* execute a rtx primitive to test */
+        g_test_fixture.release_processor();
+    }
+}
+/* third party dummy test process 3 */ 
+void test3()
+{
+    rtx_dbug_outs((CHAR *)"rtx_test: test3\r\n");
+    while (1) 
+    {
+        /* execute a rtx primitive to test */
+        g_test_fixture.release_processor();
+    }
+}
+
+/* third party dummy test process 4 */ 
+void test4()
+{
+    rtx_dbug_outs((CHAR *)"rtx_test: test4\r\n");
+    while (1) 
+    {
+        /* execute a rtx primitive to test */
+        g_test_fixture.release_processor();
+    }
+}
+/* third party dummy test process 5 */ 
+void test5()
+{
+    rtx_dbug_outs((CHAR *)"rtx_test: test5\r\n");
+    while (1) 
+    {
+        /* execute a rtx primitive to test */
+        g_test_fixture.release_processor();
+    }
+}
+/* third party dummy test process 6 */ 
+void test6()
+{
+    rtx_dbug_outs((CHAR *)"rtx_test: test6\r\n");
+    while (1) 
+    {
+        /* execute a rtx primitive to test */
+        g_test_fixture.release_processor();
+    }
+}
+
+/* register the third party test processes with RTX */
+void __attribute__ ((section ("__REGISTER_TEST_PROCS__")))register_test_proc()
+{
+    int i;
+
+    rtx_dbug_outs((CHAR *)"rtx_test: register_test_proc()\r\n");
+
+    for (i = 0; i < NUM_TEST_PROCS; i++ ) {
+        g_test_proc[i].pid = i + 1;
+        g_test_proc[i].priority = 3;
+        g_test_proc[i].sz_stack = 2048;
+    }
+    g_test_proc[0].entry = test1;
+    g_test_proc[1].entry = test2;
+    g_test_proc[2].entry = test3;
+    g_test_proc[3].entry = test4;
+    g_test_proc[4].entry = test5;
+    g_test_proc[5].entry = test6;
+}
+
+/**
+ * Main entry point for this program.
+ * never get invoked
+ */
+int main(void)
+{
+    rtx_dbug_outs((CHAR *)"rtx_test: started\r\n");
+    return 0;
+}
