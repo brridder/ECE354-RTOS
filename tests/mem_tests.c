874f154e6e99bfccd5b190a27ce1ab42bed994e4
Casey Banner <kcbanner@gmail.com>
2011-06-26 14:07:32 -0400

    * Added test for memory blocking.

--- tests/mem_tests.c
+++ tests/mem_tests.c
@@ -21,164 +21,183 @@ extern void* __end;
 void test1()
 {
     rtx_dbug_outs((CHAR *)"rtx_test: test1\r\n");
-    
-        int i;
-        int failures;
-        void* p_mem;
-        void* p_mem_array[NUM_MEM_BLKS];
+    int i;
+    int failures;
+    void* p_mem_array[NUM_MEM_BLKS];
 
+    rtx_dbug_outs((CHAR *) "\r\nBeginning memory tests...\r\n");
 
-        rtx_dbug_outs((CHAR *) "\r\nBeginning memory tests...\r\n");
-
-        //
-        // Attempt to release all memory before allocated (should fail)
-        //
+    //
+    // Attempt to release all memory before allocated (should fail)
+    //
         
-        failures = 0;
-        rtx_dbug_outs((CHAR *) "  Try releasing all memory before being allocated...\r\n");
-        for (i=0; i< NUM_MEM_BLKS; i++) {
-            int temp;
-            temp = release_memory_block( p_mem_array[i] );
+    failures = 0;
+    rtx_dbug_outs((CHAR *) "  Try releasing all memory before being allocated...\r\n");
+    for (i=0; i< NUM_MEM_BLKS; i++) {
+        int temp;
+        temp = release_memory_block( p_mem_array[i] );
 
-            //
-            // This should not be successful
-            //
+        //
+        // This should not be successful
+        //
             
-            if (temp == RTX_SUCCESS) {
-                failures++;
-            }
-        }    
-
-        if (failures > 0) {
-            rtx_dbug_outs((CHAR *) "    fail\r\n");
-        } else {
-            rtx_dbug_outs((CHAR *) "    ok\r\n");
+        if (temp == RTX_SUCCESS) {
+            failures++;
         }
+    }    
 
-        //
-        // Try allocating every block
-        // 
+    if (failures > 0) {
+        rtx_dbug_outs((CHAR *) "    fail\r\n");
+    } else {
+        rtx_dbug_outs((CHAR *) "    ok\r\n");
+    }
 
-        failures = 0;
-        rtx_dbug_outs((CHAR *) "  Try allocating every block...\r\n");
-        for (i=0; i< NUM_MEM_BLKS; i++) {
-            p_mem_array[i] = request_memory_block();
-            if (p_mem_array[i] == NULL) {
-                rtx_dbug_outs((CHAR *) "    Null pointer.\r\n");
-            } else if (p_mem_array[i] > &__end) {
-                rtx_dbug_outs((CHAR *) "    Memory out of bound. \r\n");
-            }
+    //
+    // Try allocating every block
+    // 
+
+    failures = 0;
+    rtx_dbug_outs((CHAR *) "  Try allocating every block...\r\n");
+    for (i=0; i< NUM_MEM_BLKS; i++) {
+        p_mem_array[i] = request_memory_block();
+        if (p_mem_array[i] == NULL) {
+            rtx_dbug_outs((CHAR *) "    Null pointer.\r\n");
+        } else if ((int)p_mem_array[i] > (int)&__end) {
+            rtx_dbug_outs((CHAR *) "    Memory out of bound. \r\n");
         }
+    }
 
-        if (failures > 0) {
-            rtx_dbug_outs((CHAR *) "    fail\r\n");
-        } else {
-            rtx_dbug_outs((CHAR *) "    ok\r\n");
+    if (failures > 0) {
+        rtx_dbug_outs((CHAR *) "    fail\r\n");
+    } else {
+        rtx_dbug_outs((CHAR *) "    ok\r\n");
+    }
+
+    //
+    // Try releasing all memory blocks
+    //
+
+    failures = 0;
+    rtx_dbug_outs((CHAR *) "  Try releasing all memory blocks...\r\n");
+    for (i=0; i< NUM_MEM_BLKS; i++) {
+        int temp;
+        temp = release_memory_block(p_mem_array[i]);
+        if (temp != 0 ) {
+            failures++;
         }
+    }
 
-        // 
-        // Try allocating memory after all memory was allocated, this should return NULL
-        //
+    if (failures > 0) {
+        rtx_dbug_outs((CHAR *) "    fail\r\n");
+    } else {
+        rtx_dbug_outs((CHAR *) "    ok\r\n");
+    }
+
+    //
+    // Attempt to release all memory again (should fail)
+    //
+
+    failures = 0;
+    rtx_dbug_outs((CHAR *) "  Attempting to release all memory again...\r\n");
+    for (i=0; i< NUM_MEM_BLKS; i++) {
+        int temp;
+        temp = release_memory_block( p_mem_array[i] );
+        if (temp == 0 ) {
+            failures++;
+        }
+    }    
+
+    if (failures > 0) {
+        rtx_dbug_outs((CHAR *) "    fail\r\n");
+    } else {
+        rtx_dbug_outs((CHAR *) "    ok\r\n");
+    }
+
+    //
+    // Allocate all memory, write 128 bytes to each block, deallocate, and then reallocate
+    // 
         
-        p_mem = request_memory_block();
-        if (p_mem == NULL) {
-            rtx_dbug_outs((CHAR *) "  Allocate memory block with none available...\r\n    ok\r\n");
-        } else {
-            rtx_dbug_outs((CHAR *) "  Allocate memory block with none available...\r\n    fail\r\n");
-        }
-
-        //
-        // Try releasing all memory blocks
-        //
-
-        failures = 0;
-        rtx_dbug_outs((CHAR *) "  Try releasing all memory blocks...\r\n");
-        for (i=0; i< NUM_MEM_BLKS; i++) {
-            int temp;
-            temp = release_memory_block(p_mem_array[i]);
-            if (temp != 0 ) {
-                failures++;
-            }
-        }
-
-        if (failures > 0) {
-            rtx_dbug_outs((CHAR *) "    fail\r\n");
-        } else {
-            rtx_dbug_outs((CHAR *) "    ok\r\n");
-        }
-
-        //
-        // Attempt to release all memory again (should fail)
-        //
-
-        failures = 0;
-        rtx_dbug_outs((CHAR *) "  Attempting to release all memory again...\r\n");
-        for (i=0; i< NUM_MEM_BLKS; i++) {
-            int temp;
-            temp = release_memory_block( p_mem_array[i] );
-            if (temp == 0 ) {
-                failures++;
-            }
-        }    
-
-        if (failures > 0) {
-            rtx_dbug_outs((CHAR *) "    fail\r\n");
-        } else {
-            rtx_dbug_outs((CHAR *) "    ok\r\n");
-        }
-
-        //
-        // Allocate all memory, write 128 bytes to each block, deallocate, and then reallocate
-        // 
-        
-        failures = 0;
-        rtx_dbug_outs((CHAR *) "  Allocate all memory, write 128 bytes to each block, deallocate, and then reallocate...\r\n");
-        for (i=0; i< NUM_MEM_BLKS; i++) {
-            p_mem_array[i] = request_memory_block();
+    failures = 0;
+    rtx_dbug_outs((CHAR *) "  Allocate all memory, write 128 bytes to each block, deallocate, and then reallocate...\r\n");
+    for (i=0; i< NUM_MEM_BLKS; i++) {
+        p_mem_array[i] = request_memory_block();
             
-            int j;        
-            unsigned char *current_byte = p_mem_array[i];
-            for (j=0; j < 128; j++) {
-              *current_byte = 0xFF;
-              current_byte++;
-            }
+        int j;        
+        unsigned char *current_byte = p_mem_array[i];
+        for (j=0; j < 128; j++) {
+            *current_byte = 0xFF;
+            current_byte++;
         }
+    }
 
-        for (i=0; i< NUM_MEM_BLKS; i++) {
-            int temp;
-            temp = release_memory_block(p_mem_array[i]);
-            if (temp == RTX_ERROR) {
-                rtx_dbug_outs((CHAR *) "    Failed to deallocate block\r\n");            
-                failures++;
-            }
+    for (i = 0; i < NUM_MEM_BLKS; i++) {
+        int temp;
+        temp = release_memory_block(p_mem_array[i]);
+        if (temp == RTX_ERROR) {
+            rtx_dbug_outs((CHAR *) "    Failed to deallocate block\r\n");            
+            failures++;
         }
+    }
 
-        for (i=0; i< NUM_MEM_BLKS; i++) {
-            p_mem_array[i] = request_memory_block();
+    for (i = 0; i < NUM_MEM_BLKS; i++) {
+        p_mem_array[i] = request_memory_block();
 
-            if (p_mem_array[i] == NULL || p_mem_array[i] > 0x10200000) {
-                rtx_dbug_outs((CHAR *) "    Failed to allocate block\r\n");
-                failures++;
-            }        
+        if (p_mem_array[i] == NULL || (int)p_mem_array[i] > (int)0x10200000) {
+            rtx_dbug_outs((CHAR *) "    Failed to allocate block\r\n");
+            failures++;
         }
+    }
 
-        if (failures > 0) {
-            rtx_dbug_outs((CHAR *) "    fail\r\n");
-        } else {
-            rtx_dbug_outs((CHAR *) "    ok\r\n");
-        }    
+    if (failures > 0) {
+        rtx_dbug_outs((CHAR *) "    fail\r\n");
+    } else {
+        rtx_dbug_outs((CHAR *) "    ok\r\n");
+    }    
+
+    printf_0("    Cleanup...");
+    for (i = 0; i < NUM_MEM_BLKS; i++) {
+        release_memory_block(p_mem_array[i]);
+    }
+    printf_0("...done\r\n");
 
     while (1) 
     {
-       /* execute a rtx primitive to test */
-            g_test_fixture.release_processor();
+        /* execute a rtx primitive to test */
+        g_test_fixture.release_processor();
     }
 }
 
 /* third party dummy test process 2 */ 
 void test2()
 {
+    int i;
+    void* p_mem_array[NUM_MEM_BLKS];
+
     rtx_dbug_outs((CHAR *)"rtx_test: test2\r\n");
+    printf_0("Beginning memory blocked queue test...\r\n");
+
+    //
+    // Allocate every block
+    // 
+
+    printf_0("  Process 2: Allocate every block...");
+    for (i = 0; i < NUM_MEM_BLKS; i++) {
+        p_mem_array[i] = request_memory_block();
+    }
+    printf_0("...done - releasing processor\r\n");
+    g_test_fixture.release_processor();
+
+    printf_0("  Process 2: Release a block...");
+    release_memory_block(p_mem_array[0]);
+    printf_0("...done - releasing processor\r\n");
+    g_test_fixture.release_processor();
+
+    printf_0("  Process 2: Cleanup...");
+    for (i = 1; i < NUM_MEM_BLKS; i++) {
+        release_memory_block(p_mem_array[i]);
+    }
+    printf_0("...done\r\n");
+
     while (1) 
     {
         /* execute a rtx primitive to test */
@@ -188,7 +207,14 @@ void test2()
 /* third party dummy test process 3 */ 
 void test3()
 {
-    rtx_dbug_outs((CHAR *)"rtx_test: test3\r\n");
+    void* block;
+
+    printf_0("  Process 3: Try to allocate a block...\r\n");
+    block = request_memory_block();
+    printf_1("  Process 3: Got a block: %x - releasing it...", block);
+    release_memory_block(block);    
+    printf_0("done\r\n");
+
     while (1) 
     {
         /* execute a rtx primitive to test */
