309c03d72e9b9a5899c23bb50bfa275b2c1cd38a
Ben Ridder <ben@xtremelabs.com>
2011-07-03 13:36:39 -0400

    * - missed some boiler platte

--- core/init.c
+++ core/init.c
@@ -269,16 +269,11 @@ void  __attribute__ ((section ("__REGISTER_RTX__"))) register_rtx() {
     g_test_fixture.release_processor = release_processor;
     g_test_fixture.set_process_priority = set_process_priority;
     g_test_fixture.get_process_priority = get_process_priority;
-
-    //
-    // TODO: Implement required OS functions
-    //
-     
     g_test_fixture.send_message = send_message;
     g_test_fixture.receive_message = receive_message;
     g_test_fixture.request_memory_block = request_memory_block;
     g_test_fixture.release_memory_block = release_memory_block;
-    //g_test_fixture.delayed_send = delayed_send;
+    g_test_fixture.delayed_send = delayed_send;
 
     rtx_dbug_outs((CHAR *)"rtx: leaving register_rtx()\r\n");
 }
