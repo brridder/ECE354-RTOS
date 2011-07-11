957f94145c8f85ff57aae577cdd8f209a25037d4
Ben Ridder <ben@xtremelabs.com>
2011-07-10 22:22:50 -0400

    * commented up the user processes

--- processes/user_processes.c
+++ processes/user_processes.c
@@ -11,12 +11,18 @@ void proc_a() {
     int num;
     
     num = 0;
+    //
+    // Register the command: %Z
+    //
     message = (message_envelope*)request_memory_block(); 
     message->type = MESSAGE_CMD_REG;
     str_cpy(message->data, (char*)command);
     send_message(KCD_PID, message);
 
     while(1) {
+        //
+        // Loop until we get %Z  (AKA, idle until called upon)
+        //
         message = (message_envelope*)receive_message(0);
         if (str_cmp(message->data, command)) {
             release_memory_block(message);
@@ -27,6 +33,9 @@ void proc_a() {
     }
     
     while(1) {
+        //
+        // Send messages to process B forever.
+        //
         message = (message_envelope*)request_memory_block();
         message->type = MESSAGE_COUNT_REPORT;
         memcpy(message->data, &num, sizeof(num));
@@ -39,6 +48,9 @@ void proc_a() {
 void proc_b() {
     message_envelope* message;
     while(1) {
+        //
+        // Forward messages to process C
+        //
         message = (message_envelope*)receive_message(0);
         send_message(PROC_C, message);
     }
@@ -55,14 +67,32 @@ void proc_c() {
     int message_data;
 
     while(1) {
+        //
+        // If local message queue is null, get another message from the kernel
+        //
         if (queue.head == NULL) {
             message_in = (message_envelope*)receive_message(0);
         } else {
             message_in = queue_dequeue_m(&queue); 
         }
 
+        //
+        // Check message type
+        //
         if (message_in->type == MESSAGE_COUNT_REPORT) {
+
+            //
+            // Copy the message data into a temp buffer
+            // 
+            
             memcpy(&message_data, message_in->data, sizeof(message_data));
+
+            //
+            // If the counter is divisible by 20, print out 'PROCESS C' and
+            // delay the next loop by 10 seconds while enqueueing all incoming
+            // messages
+            //
+            
             if (message_data % 20 == 0 && message_data != 0) {
                 message_in->type = MESSAGE_KEY_INPUT;
                 str_cpy(message_in->data, (char*)output);
@@ -82,6 +112,10 @@ void proc_c() {
                 }
             }
         }
+
+        //
+        // Release the recieved message
+        //
 
         error_code = release_memory_block(message_in);
 
